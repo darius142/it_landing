cc8af3d85848628738044181e9171f3da8f18421
Volodymyr Padovsky <vld06@ukr.net>
2017-09-14 16:36:49 +0300

    Simple validation was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,24 @@
 (function() {
-	// Add event listener for open form button
-	var openFormButton =document.querySelector('.arrow-down');
+    var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
 
-	if(openFormButton) {
-		openFormButton.addEventListener('click', function(e) {
-			e.preventDefault();
-			form.open();
-		})
-	}
+    if (openFormButton) {
+        openFormButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            ITVDN.form.open();
+        })
+    }
+
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (ITVDN.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+
+        })
+    }
 }());
